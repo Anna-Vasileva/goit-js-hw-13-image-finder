1edf252c2a30ef80ace405be899b43dc85c50f8f
Anna Vasileva <anette.vasileva@gmail.com>
2021-09-17 14:43:26 +0300

    убирает комментарии, корректирует кнопку "load more"

--- src/apiService.js
+++ src/apiService.js
@@ -1,19 +1,3 @@
-// const KEY = '23417274 - c745cca46d265f1806e9566e8';
-// q - что искать
-// page=номер_страницы
-
-// export default async function getImages({ q, key, page }) {
-//   try {
-//     const response = await fetch(
-//       `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${q}&page=${page}&per_page=12&key=${key}`,
-//     );
-//     // console.log(response);
-//     const images = response.json;
-//     // console.log(images);
-//   } catch (error) {
-//     console.log('ошибочка!');
-//   }
-// }
 export default class newsApiService {
   constructor() {
     this.searchQuery = '';
@@ -28,12 +12,8 @@ export default class newsApiService {
         `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`,
       );
 
-      //   console.log(response);
-
       const images = response.json();
       this.incrementPage();
-
-      //   console.log(images);
 
       return images;
     } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import './sass/main.scss';
 import gallery from './template/gallery.hbs';
 import newsApiService from './apiService';
+import loadMoreBtn from './btn-load-more';
+
+const galleryApiService = new newsApiService();
+const btnLoadMore = new loadMoreBtn();
 
 const refs = {
   ulEl: document.querySelector('.gallery'),
   btnSearch: document.querySelector('.btn-search'),
   formEl: document.querySelector('.search-form'),
-  btnLoadMore: document.querySelector('.btn-load'),
+  btnLoadMore: btnLoadMore.button,
 };
-const galleryApiService = new newsApiService();
 
 refs.formEl.addEventListener('submit', onSearch);
 refs.btnLoadMore.addEventListener('click', loadMore);
@@ -21,17 +24,8 @@ function onSearch(e) {
   galleryApiService.fetchImages().then(images => {
     const cardImages = gallery(images.hits);
     refs.ulEl.innerHTML = cardImages;
+    btnLoadMore.show();
   });
-
-  //   data.q = e.currentTarget.elements.query.value;
-  //   console.log('onSearch');
-
-  //   getImages(data).then(images => {
-  //     const cardImages = gallery(images);
-  //     console.log(cardImages);
-
-  //     refs.ulEl.innerHTML = cardImages;
-  //   });
 }
 function loadMore(e) {
   e.preventDefault();
@@ -45,15 +39,4 @@ function loadMore(e) {
       block: 'end',
     });
   });
-
-  //   console.log('loadMore');
-
-  //   getImages(data).then(images => {
-  //     const cardImages = gallery(images);
-  //     console.log(cardImages);
-
-  //     refs.ulEl.innerHTML = cardImages;
-  //   });
 }
-
-// const element = document.getElementById('.my-element-selector');
